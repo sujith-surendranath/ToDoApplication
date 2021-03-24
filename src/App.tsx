9416a27daf8710a5c684f35e00fc3428427c81b8
Sujith Surendranath <susurendranath@deloitte.com>
2021-03-24 12:55:28 +0530

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [greetUser, setGreetUser] = useState("");
+  const [username, setUsername] = useState(""); // text input
+
+  const onButtonClick = () => {
+    // alert("Hi");
+    // setName("Hi " + name);
+    setGreetUser(`Hi ${username}`);
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    setUsername(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p>{greetUser}</p>
+      <input
+        type="text"
+        name="userName"
+        onChange={handleInput}
+        value={username}
+      />
+      <button onClick={onButtonClick}>Greet the user</button>
     </div>
   );
 }
